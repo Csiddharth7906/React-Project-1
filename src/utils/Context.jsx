52155adc60de3c9d9c072a64c962ef7acd37d97b
Siddharth Chauhan <158046791+Csiddharth7906@users.noreply.github.com>
2025-07-29 22:31:13 +0530

    Update Context.jsx

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -6,18 +6,18 @@ import React, { createContext, useEffect, useState } from 'react'
 export const ProductContext = createContext();
 const Context = (props) => {
   const [products,setProducts] = useState(JSON.parse(localStorage.getItem("products"))||null);
-  // const getProducts = async () => {
-  //   try{
-  //       const {data} = await axios("/products")
-  //       setProducts(data);
-  //       console.log(data);
-  //   }catch(error){
-  //       console.error(error);
-  //   }
-  // };
-  // useEffect(() => {
-  //   getProducts();
-  // },[])
+  const getProducts = async () => {
+    try{
+        const {data} = await axios("/products")
+        setProducts(data);
+        console.log(data);
+    }catch(error){
+        console.error(error);
+    }
+  };
+  useEffect(() => {
+    getProducts();
+  },[])
   return (
     <ProductContext.Provider value={[products,setProducts]}> 
                 {props.children}
